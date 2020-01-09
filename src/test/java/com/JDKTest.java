cb21cb3f3864c7a1ae237cdb0a0cd79f99cf35cd
mengqh <1615404392@qq.com>
2020-01-09 21:54:30 +0800

    增加Test类

--- src/test/java/com/JDKTest.java
+++ src/test/java/com/JDKTest.java
@@ -3,6 +3,7 @@ package com;
 import com.xrltao.po.User;
 import com.xrltao.po.Video;
 import com.xrltao.provider.VideoProvider;
+import com.xrltao.util.HttpCilentUtil;
 import com.xrltao.util.JwtUtils;
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
@@ -15,8 +16,7 @@ import io.jsonwebtoken.Jwts;
  */
 public class JDKTest {
     public static void main(String[] args) {
-        Claims claims = JwtUtils.parserToken("eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ4cmx0YW8iLCJpZCI6MTIyMjIyLCJuYW1lIjoiYWFhYWFhYWEiLCJoZWFkSW1nIjoiYWFhYWFhYSIsImlhdCI6MTU3ODMxOTg4MSwiZXhwIjoxNTc4NDA2MjgxfQ.DPtHo-OcrKdO4lgMDqqRNw8KNW8IU5bpq-CzdjyS0Jw");
-        System.out.println(claims.get("namxzdfe"));
+       // System.out.println();
 
     }
 }
